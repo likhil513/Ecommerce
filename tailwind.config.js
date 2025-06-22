e97796aadc3ae98878115aa8cdea34fb1e3e84a1
Likhil-dev <likhil.g@haystek.com>
2025-06-22 11:15:02 +0530

    Ecommerce Day-2

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",   // If using React or TS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
