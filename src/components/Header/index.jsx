8e3891c26a3ccae20eb2c91005733d5623e45963
likhil513 <likhil513@gmail.com>
2025-06-29 16:19:17 +0530

    Header completion

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import Search from '../Search'
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { TbGitCompare } from "react-icons/tb";
+import Button from '@mui/material/Button';
+import { FaRegHeart } from "react-icons/fa";
+import Tooltip from '@mui/material/Tooltip';
+
+
+
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    right: -3,
+    top: 13,
+    border: `2px solid ${(theme.vars ?? theme).palette.background.paper}`,
+    padding: '0 4px',
+  },
+}));
+
 
 const Header = () => {
   return (
@@ -28,9 +49,39 @@ const Header = () => {
           </div>
           <div className="col3 w-[30%]">
             <div className='flex justify-end items-center gap-6'>
-              <Link to='/wishlist' className='text-[16px] text-brand-dark hover:text-brand-blue transistion'>Wishlist</Link>
-              <Link to='/cart' className='text-[16px] text-brand-dark hover:text-brand-blue transistion'>Cart</Link>
-              <Link to='/login' className='text-[16px] text-brand-dark hover:text-brand-blue transistion'>Login</Link>
+              <div>
+                <Button component={Link} to="/login" className='!bg-brand-light !text-brand-dark !p-2 hover:!bg-brand-sky !font-medium'>Login</Button>
+                <Button component={Link} to="/register" className='!bg-brand-light !text-brand-dark !ml-2 !p-2  hover:!bg-brand-sky'>Register</Button>
+              </div>
+              <ul className='flex gap-3 list-style-none'>
+                <li>
+                  <Tooltip title="Compare">
+                    <IconButton aria-label="compare">
+                      <StyledBadge badgeContent={4} color="secondary">
+                        <TbGitCompare />
+                      </StyledBadge>
+                    </IconButton>
+                  </Tooltip>
+                </li>
+                <li>
+                  <Tooltip title="Favorites">
+                    <IconButton aria-label="favorites">
+                      <StyledBadge badgeContent={4} color="secondary">
+                        <FaRegHeart />
+                      </StyledBadge>
+                    </IconButton>
+                  </Tooltip>
+                </li>
+                <li>
+                  <Tooltip title="Cart">
+                    <IconButton aria-label="Cart">
+                      <StyledBadge badgeContent={4} color="secondary">
+                        <MdOutlineShoppingCart />
+                      </StyledBadge>
+                    </IconButton>
+                  </Tooltip>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
